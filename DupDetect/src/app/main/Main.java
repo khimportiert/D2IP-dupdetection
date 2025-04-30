69c8852d0d4cc6852c3005b303277052bf3463e9
DMLoennecker <Dmloennecker@campus.tu-berlin.de>
2025-04-30 18:51:40 +0200

    f1 take 1

--- DupDetect/src/app/main/Main.java
+++ DupDetect/src/app/main/Main.java
@@ -18,15 +18,16 @@ public class Main {
         String currentDir = System.getProperty("user.dir");
         CSVReader fr = new CSVReader(new File(currentDir + "/data/Z1.csv").getAbsolutePath());
         CSVReader frSolution = new CSVReader(new File(currentDir + "/data/ZY1.csv").getAbsolutePath());
+        CSVReader frSolution2 = new CSVReader(new File(currentDir + "/data/ZY2.csv").getAbsolutePath());
 
         StopWatch.start();
 
         //LinkedList<Notebook> notebooks = fr.read(Notebook.class);
-        LinkedList<Dup> dup = frSolution.read(Dup.class);
+        LinkedList<Dup> dups1 = frSolution.read(Dup.class);
+        LinkedList<Dup> dups2 = frSolution.read(Dup.class);
+        System.out.println("f1 score: " + FilterTesting.evaluateF1(dups2, dups2));
 
-        FilterTesting.evaluateF1(dup, dup);
 
-        SampleDupPrint.showSamples(50,150);
 
         StopWatch.stop();
     }
