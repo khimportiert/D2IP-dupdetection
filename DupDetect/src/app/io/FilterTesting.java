c272331abaf78f31a7d79cb2b26842584188312b
Schulien-J <jj.konrad.de@gmail.com>
2025-04-30 21:16:57 +0200

    Khimmyyyyyyyyyy

--- DupDetect/src/app/io/FilterTesting.java
+++ DupDetect/src/app/io/FilterTesting.java
@@ -1,28 +1,49 @@
 package app.io;
 
+import app.misc.StopWatch;
 import app.model.Dup;
 import app.model.Notebook;
 
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Set;
+import java.util.*;
 
 public class FilterTesting {
-    public static Dup findDups(LinkedList<Notebook> notebooks)
+    public static LinkedList<Dup> findDups(LinkedList<Notebook> notebooks)
     {
+        LinkedList<Dup> foundDups = new LinkedList<>();
+
         int n = notebooks.size();
+
+        ArrayList<String> stringArr = new ArrayList<String>();
+        for(int i = 0; i < n; i++)
+        {
+            stringArr.add(notebooks.get(i).getTitle().trim().toLowerCase());
+
+        }
+
         for (int i = 0; i < n; i++)
         {
             for(int j = 0; j < i; j++)
             {
-                String notebookI = notebooks.get(i).getTitle();
-                String notebookJ = notebooks.get(j).getTitle();
+                String notebookI = stringArr.get(i);
+                String notebookJ = stringArr.get(j);
 
-
+                if (notebookI.equals(notebookJ))
+                {
+                    Dup newDup = new Dup();
+                    newDup.setIds(i,j);
+                    foundDups.add(newDup);
+                }
+            }
+            if(i%100 == 0) {
+                System.out.println("i: " + i);
+                StopWatch.peek();
             }
         }
 
-        return null;
+        return foundDups;
     }
 
     public static double evaluateF1(LinkedList<Dup> trueDups, LinkedList<Dup> declaredDups)
